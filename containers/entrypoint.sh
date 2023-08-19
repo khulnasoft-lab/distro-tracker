0ccf53f5ef7f8eff52129cbe110a095a5b1c6678
Raphaël Hertzog <hertzog@debian.org>
2023-08-19 19:30:23 +0200

    container: create the required database on startup

--- containers/entrypoint.sh
+++ containers/entrypoint.sh
@@ -5,6 +5,7 @@ set -x
 # Start postgresql for the benefit of everything
 /etc/init.d/postgresql start
 su postgres - -c "createuser -d root"
+su postgres - -c "createdb distro-tracker -O root -l C.UTF-8"
 
 # Setup the database for the Django application
 if [ -e /app/manage.py ]; then
